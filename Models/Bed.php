ebddb0d5fb6df53f68563a168c63a2f80c8d0810
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-20 16:03:08 +0100

    Creata classe Bed

---
+++ Models/Bed.php
@@ -0,0 +1,47 @@
+<?php
+
+class Game extends Product
+{
+    private $material;
+    private $sizes;
+
+    public function __construct(
+        $name,
+        $pic,
+        $price,
+        $description,
+        $brand,
+        $material,
+        $sizes
+    ) {
+        parent::__construct($name, $pic, $price, $description, $brand);
+        $this->setMaterial(trim($material));
+        $this->setSizes(trim($sizes));
+    }
+
+    public function setMaterial($material)
+    {
+        if (!$material && is_numeric($material)) return false;
+        $this->material = $material;
+    }
+
+    public function getMaterial()
+    {
+        return $this->material;
+    }
+
+    public function setSizes($sizes)
+    {
+        if (!$sizes && gettype($sizes) !== 'array') return false;
+        $this->sizes = $sizes;
+    }
+
+    public function getSizes()
+    {
+        $sizes = '';
+        foreach ($this->sizes as $size) {
+            $sizes .= "$size cm. x ";
+        }
+        return substr($sizes, 0, -3);
+    }
+}
